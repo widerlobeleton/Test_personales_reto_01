b5cf91d5bf8cc713152aee3975664c1a06d5d7df
widerlobeleton <j.navarrof@uniandes.edu.co>
2026-02-17 18:28:32 -0500

    cambios en readme y en logic carga de data y time

--- App/logic.py
+++ App/logic.py
@@ -4,10 +4,61 @@ def new_logic():
     """
     Crea el catalogo para almacenar las estructuras de datos
     """
+    inicio = time.time()
     #TODO: Llama a las funciónes de creación de las estructuras de datos
-    pass
-
-
+    data = open("computer_prices_small.csv","r", encoding ="utf-8")
+    titulos = data.readline()
+    print (titulos)
+    diccionario = {}
+    line = data.readline()
+    while len(line) > 0:
+        datos = linea.strip().split(",") 
+        diccionario_sec = {
+            
+        }
+        linea = data.readline()
+        device_type = datos[0]
+        diccionario_sec["brand"] = datos[1]
+        diccionario_sec["model"] = datos[2]
+        diccionario_sec["release_year"] = datos[2]
+        diccionario_sec["os"] = datos[4]
+        diccionario_sec["form_factor"] = datos[5]
+        diccionario_sec["operative_sistem"] = datos[6]
+        diccionario_sec["form_factor"] = datos[7]
+        diccionario_sec["cpu_brand"] = datos[8]
+        diccionario_sec["cpu_model"] = datos[9]
+        diccionario_sec["cpu_tier"] = datos[10]
+        diccionario_sec["cpu_cores"] = datos[11]
+        diccionario_sec["cpu_threads"] = datos[12]
+        diccionario_sec["cpu_base_ghz"] = datos[13]
+        diccionario_sec["cpu_boost_ghz"] = datos[14]
+        diccionario_sec["gpu_brand"] = datos[14]
+        diccionario_sec["gpu_tier"] = datos[15]
+        diccionario_sec["vram_gb"] = datos[16]
+        diccionario_sec["ram_gb"] = datos[17]
+        diccionario_sec["storage_type"] = datos[18]
+        diccionario_sec["storage_gb"] = datos[19]
+        diccionario_sec["storage_drive_count"] = datos[20]
+        diccionario_sec["display_type"] = datos[21]
+        diccionario_sec["display_size"] = datos[22]
+        diccionario_sec["resolution"] = datos[23]
+        diccionario_sec["refresh_hz"] = datos[24]
+        diccionario_sec["battery_wh"] = datos[25]
+        diccionario_sec["charger_watts"] = datos[26]
+        diccionario_sec["psu_watts"] = datos[27]
+        diccionario_sec["wifi"] = datos[28]
+        diccionario_sec["weight_kg"] = datos[30]
+        diccionario_sec["warranty_months"] = datos[31]
+        diccionario_sec["price"] = datos[32]
+        if device_type not in diccionario:
+            diccionario[device_type] = []
+        diccionario[device_type].append(diccionario_sec)
+        linea = data.readline()
+    data.close()
+    final = time.time()
+    tiempo = final - inicio
+    return diccionario, tiempo
+        
 # Funciones para la carga de datos
 
 def load_data(catalog, filename):
--- README.md
+++ README.md
@@ -32,9 +32,9 @@ This repository is part of the data structure and algorithms (#EDA) teaching fra
 
 The students edit this section to add their names, Uniandes emails, and specify which project functionality of the project they will implement.
 
-1. Student No. 1 Name, Student No. 1 Uniandes Email, Student No. 1 owned functionality.
-1. Student No. 2 Name, Student No. 2 Uniandes Email, Student No. 2 owned functionality.
-1. Student No. 3 Name, Student No. 3 Uniandes Email, Student No. 3 owned functionality.
+1. Juan David Saldaña Mateus , J.saldanam@uniandes.edu.co, 202511865 Funcionalidad propiedad del estudiante No. 1.
+2. Juan David Navarro Florez, j.navarro@uniandes.edu.co, 202526487 Funcionalidad propiedad del estudiante No. 2.
+3. Everth Andres Plata Echeverry , e.platae@uniandes.edu.co , 202513367 Funcionalidad propiedad del estudiante No. 3.
 
 [Back to top](#retos-template)
 
